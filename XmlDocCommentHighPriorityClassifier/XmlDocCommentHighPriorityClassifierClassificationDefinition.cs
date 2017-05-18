caba27eddf619260d9f96613d360a59134cdfa57
Nick Halvorsen <nhalvorsen@webstaurantstore.com>
2017-05-18 12:49:32 -0400

    Initial implementation

---
+++ XmlDocCommentHighPriorityClassifier/XmlDocCommentHighPriorityClassifierClassificationDefinition.cs
@@ -0,0 +1,30 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="XmlDocCommentHighPriorityClassifierClassificationDefinition.cs" company="Company">
+//     Copyright (c) Company.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Utilities;
+
+namespace XmlDocCommentHighPriorityClassifier
+{
+    /// <summary>
+    /// Classification type definition export for XmlDocCommentHighPriorityClassifier
+    /// </summary>
+    internal static class XmlDocCommentHighPriorityClassifierClassificationDefinition
+    {
+        // This disables "The field is never used" compiler's warning. Justification: the field is used by MEF.
+#pragma warning disable 169
+
+        /// <summary>
+        /// Defines the "XmlDocCommentHighPriorityClassifier" classification type.
+        /// </summary>
+        [Export(typeof(ClassificationTypeDefinition))]
+        [Name("XmlDocCommentHighPriorityClassifier")]
+        private static ClassificationTypeDefinition typeDefinition;
+
+#pragma warning restore 169
+    }
+}
---
+++ XmlDocCommentHighPriorityClassifier/XmlDocCommentHighPriorityClassifierFormat.cs
@@ -0,0 +1,34 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="XmlDocCommentHighPriorityClassifierFormat.cs" company="Company">
+//     Copyright (c) Company.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+using System.ComponentModel.Composition;
+using System.Windows.Media;
+using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Utilities;
+
+namespace XmlDocCommentHighPriorityClassifier
+{
+    /// <summary>
+    /// Defines an editor format for the XmlDocCommentHighPriorityClassifier type that has a purple background
+    /// and is underlined.
+    /// </summary>
+    [Export(typeof(EditorFormatDefinition))]
+    [ClassificationType(ClassificationTypeNames = "XmlDocCommentHighPriorityClassifier")]
+    [Name("XmlDocCommentHighPriorityClassifier")]
+    [UserVisible(true)] // This should be visible to the end user
+    [Order(After = Priority.High)] // Set the priority to be above the default classifiers. Necessary for uniform text coloring
+    internal sealed class XmlDocCommentHighPriorityClassifierFormat : ClassificationFormatDefinition
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="XmlDocCommentHighPriorityClassifierFormat"/> class.
+        /// </summary>
+        public XmlDocCommentHighPriorityClassifierFormat()
+        {
+            this.DisplayName = "XML Doc Comment (High priority)";
+            this.ForegroundColor = Colors.LightGray;
+        }
+    }
+}
